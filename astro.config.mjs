213279aa36568e8b3710e60ac6e2758bf537ea4d
Gabriel Aedo <aedothegabriel@gmail.com>
2026-02-15 14:46:30 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: 'https://GabrielAedoPozo.github.io',
+  base: '/marc_chirinos/',
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
